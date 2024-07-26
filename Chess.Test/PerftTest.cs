e07644e4c7b88b5f837198335cb4570459b48c47
Peter Rasmussen <pmrrasmussen@icloud.com>
2024-07-26 13:33:33 +0200

    Small change

--- Chess.Test/PerftTest.cs
+++ Chess.Test/PerftTest.cs
@@ -21,12 +21,12 @@ public class PerftTest
     [InlineData(1)]
     [InlineData(2)]
     [InlineData(3)]
-    [InlineData(4)] // this test is slooooow
-    [InlineData(5)] // this test is slooooow
-    [InlineData(6)] // this test is slooooow
-    [InlineData(7)] // this test is slooooow
-    [InlineData(8)] // this test is slooooow
-    [InlineData(9)] // this test is slooooow
+    [InlineData(4)]
+    [InlineData(5)]
+    [InlineData(6)]
+    [InlineData(7)]
+    [InlineData(8)]
+    [InlineData(9)]
     public void Check(int depth)
     {
         var board = BoardBuilder.GetDefaultStartingPosition();
