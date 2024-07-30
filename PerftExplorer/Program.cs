8c0dea148aee4e38daaae881e6c8bcd75e949f44
Peter Rasmussen <pmrrasmussen@icloud.com>
2024-07-30 21:25:44 +0200

    Start perft explorer

---
+++ PerftExplorer/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Chess.Builders;
+using Chess.Structs;
+
+if (!int.TryParse(args[0], out var depth))
+    Console.WriteLine("Must pass an integer for the depth argument.");
+
+var fen = args[1];
+
+var moves = args[2..].Select(moveString => new Move(moveString));
+
+var board = BoardBuilder.FromFen(fen);
