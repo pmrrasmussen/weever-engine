1d39c5451a5039e2b3ecfe5e97f161fa5c28fb99
Peter Rasmussen <pmrrasmussen@icloud.com>
2024-08-16 20:47:25 +0200

    Small change

--- Chess.Test/PerftTest.cs
+++ Chess.Test/PerftTest.cs
@@ -808,7 +808,7 @@ public class PerftTest
     [InlineData("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 6, 6923051137)]
     public void CheckFromFen(string fen, int depth, long expectedNodes)
     {
-        if (depth > 6)
+        if (depth > 5)
             return;
 
         // Arrange
